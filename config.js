a51886ec87fb6faa3c2edc6c40b2d888810a5e4a
anonys46 <lemanhcuong0406@gmail.com>
2022-12-28 01:37:55 +0700

    up

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+titleWeb: "WEB chi chi á",
+    introTitle: 'NGUYỄN THỊ QUỲNH LƯU',
+    introDesc: `Trái đất vốn lạ thường
+    Mà sao em cứ đi nhầm đường
+    Lạc vào tim anh lẻ loi
+    Đằng sau chữ yêu đây là thương`,
+    btnIntro: '^^Mời Vào^^',
+    title: 'Do You Hate Me ?',
+    desc: 'Trả lời!',
+    btnYes: 'Không hề ghét',
+    btnNo: 'ghét v :))',
+    question: 'Tại sao lại giả vờ nói ghét',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Ghét của nào trời trao của ấy :)))))))))))))))))',
+    mess: 'Mình biết mà 🥰. L bạn nhiều nhiều 😘😘',
+    messDesc: 'Đừng lừa nựa nha',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.instagram.com/direct/inbox/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
+}
